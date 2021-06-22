63d00040f409a0892db50807ebc2296116b9558e
SHILPA <andavarapushilpa95@gmail.com>
2021-06-22 19:05:25 +0200

    Part1 & part2 (except-csrf)

--- test-db.js
+++ test-db.js
@@ -1,16 +1,28 @@
-const { createSignature, getSignature } = require("./db");
+//const { response } = require("express");
+const {
+    createSignature,
+    getSignatures,
+    getSignatureById,
+    getCount,
+} = require("./db");
 
-createSignature({
-    first_name: "shilpa",
-    last_name: "roshan",
-    signatures: "something",
-})
-    .then(() => {
-        console.log("saved!");
-    })
-    .then(() => {
-        getSignature().then((signatures) => {
-            // should contain the signature you just created
-            console.log(signatures);
-        });
-    });
+// createSignature({
+//     first_name: "shilpa",
+//     last_name: "roshan",
+//     signature: "something",
+// })
+//     .then(() => {
+//         console.log("saved!");
+//         response.cookie("alreadySigned", true);
+//         response.redirect("thankyou page");
+//     })
+//     .then(() => {
+//         getSignatures().then((signatures) => {
+//             // should contain the signature you just created
+//             console.log(signatures);
+//         });
+//     });
+Promise.all([getSignatureById(1), getCount()]).then(([signature, count]) => {
+    console.log(signature, count);
+});
+//
